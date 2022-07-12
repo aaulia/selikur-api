dc38f8f8d2fc2efc1bc93b794629f7e86426887a
Ahmad Aulia Noorhakim <aaulia@gmail.com>
2022-07-12 23:01:12 +0700

    Adding skrape dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val skrape_version: String by project
 
 plugins {
     application
@@ -31,6 +32,8 @@ dependencies {
     implementation("io.ktor:ktor-locations:$ktor_version")
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("it.skrape:skrapeit:1.2.1")
 
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
